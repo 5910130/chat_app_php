32cf228bf71615539f07d1c43dc834e6922a15ae
Pawan <pawan@gmail.com>
2021-07-27 10:07:44 +0530

    message

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,7 @@
   ?>
 
   <body class="app sidebar-mini">
+
     <!-- Navbar-->
     <header class="app-header"><a class="app-header__logo" href="index.html">Vali</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
